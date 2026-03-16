b6d2d44926cac8a0721da8199e2907ff530dd195
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-03-16 16:52:20 +0200

    Add: Active tab

--- src/modules/auth/pages/AuthPage.tsx
+++ src/modules/auth/pages/AuthPage.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { LeftPanel, RightPanel } from "../components";
 import styles from "./AuthPage.module.css";
 
 export const AuthPage = () => {
+  const [activeTab, setActiveTab] = useState<"signin" | "signup">("signin");
   return (
     <div className={styles.page}>
       <LeftPanel />
-      <RightPanel />
+      <RightPanel activeTab={activeTab} onTabChange={setActiveTab} />
     </div>
   );
 };
