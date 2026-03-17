9bd3b8fccf696202289ab5915fd6a77df2f099c5
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-03-17 14:37:40 +0200

    feat: implement DTOs and mappers for login and register request

---
+++ src/modules/auth/dto/AuthRequest.ts
@@ -0,0 +1,12 @@
+export interface LoginRequestDto {
+  username: string;
+  password: string;
+}
+
+export interface RegisterRequestDto {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  image: File;
+}
