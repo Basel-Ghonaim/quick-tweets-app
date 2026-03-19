95c863af8d61dfddca0a0d55158b1983aa6ff7b9
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-03-19 16:25:07 +0200

    refactor: edit type and interface architecture

--- src/modules/auth/mapper/Mapper.ts
+++ src/modules/auth/mapper/Mapper.ts
@@ -1,4 +1,4 @@
-import type { LoginCredentials, RegisterCredentials } from "../../types";
+import type { LoginCredentials, RegisterCredentials } from "../types";
 import type {
   LoginRequestDto,
   RegisterRequestDto,
