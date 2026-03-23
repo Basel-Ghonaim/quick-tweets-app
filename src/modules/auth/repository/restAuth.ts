608abd4e95089ad0bae69eb9bd499483d0fa4884
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-03-23 16:25:13 +0200

    feat: implement register method and integrate request mapping

--- src/modules/auth/repository/restAuth.ts
+++ src/modules/auth/repository/restAuth.ts
@@ -3,13 +3,22 @@ import { apiClient } from "../../../shared/api";
 import { authMapper } from "../mapper";
 
 export const restAuth = (): AuthRepository => {
-  const { toAuthResponse, loginCredentialsToDto } = authMapper();
+  const { toAuthResponse, loginCredentialsToDto, registerCredentialsToDto } =
+    authMapper();
 
   return {
     login: async (credintials) => {
       const res = await apiClient.post(
         "/login",
         loginCredentialsToDto(credintials),
+      );
+      return toAuthResponse(res.data);
+    },
+    register: async (credintials) => {
+      const res = await apiClient.post(
+        "/register",
+        registerCredentialsToDto(credintials),
+        { headers: { "Content-Type": "multipart/form-data" } },
       );
       return toAuthResponse(res.data);
     },
