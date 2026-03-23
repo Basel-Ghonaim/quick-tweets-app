b50fe1e3ba55de3d0778e3c569c19979867b48ca
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-03-23 14:20:52 +0200

    refactor: integrate loginCredentialsToDto in restAuth repository

--- src/modules/auth/repository/restAuth.ts
+++ src/modules/auth/repository/restAuth.ts
@@ -3,11 +3,14 @@ import { apiClient } from "../../../shared/api";
 import { authMapper } from "../mapper";
 
 export const restAuth = (): AuthRepository => {
-  const { toAuthResponse } = authMapper();
+  const { toAuthResponse, loginCredentialsToDto } = authMapper();
 
   return {
     login: async (credintials) => {
-      const res = await apiClient.post("/login", credintials);
+      const res = await apiClient.post(
+        "/login",
+        loginCredentialsToDto(credintials),
+      );
       return toAuthResponse(res.data);
     },
   };
