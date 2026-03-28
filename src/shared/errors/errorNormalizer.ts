e339deb277b5aa95f0961a5bc39741d5f10df462
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-03-28 15:02:34 +0300

    refactor(shared): standardize error factory and normalizer

--- src/shared/errors/errorNormalizer.ts
+++ src/shared/errors/errorNormalizer.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import type { AppError } from "./AppError";
-import { createError } from "./errorFactory";
+import { createAppError, createUnknownError } from "./errorFactory";
 
 export const errorNormalizer = (error: unknown): AppError => {
   if (axios.isAxiosError(error)) {
@@ -8,23 +8,26 @@ export const errorNormalizer = (error: unknown): AppError => {
 
     switch (code) {
       case "ERR_CANCELED":
-        return createError("canceled", "Request was canceled");
+        return createAppError("canceled", "Request was canceled");
       case "ERR_NETWORK":
-        return createError("network", "Network error occurred");
+        return createAppError("network", "Network error occurred");
       case "ECONNABORTED":
-        return createError("timeout", "Request timeout");
+        return createAppError("timeout", "Request timeout");
       default:
         if (response) {
           const { status } = response;
           if (status === 401)
-            return createError("unauthorized", "Unauthorized access", status);
+            return createAppError(
+              "unauthorized",
+              "Unauthorized access",
+              status,
+            );
 
           if (status >= 500)
-            return createError("server", "Server error occurred", status);
+            return createAppError("server", "Server error occurred", status);
         }
     }
   }
-  if (error instanceof Error)
-    return createError("unknown", error.message, undefined, error);
-  return createError("unknown", "An unknown error occurred");
+
+  return createUnknownError(error);
 };
