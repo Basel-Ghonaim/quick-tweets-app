0955891f87590bd3c8a87f19b706a5a4dad6378e
Basel-Ghonaim <139078586+Basel-Ghonaim@users.noreply.github.com>
2026-04-02 13:06:41 +0300

    feat: add types

---
+++ src/shared/design-system/components/Button/Button.types.ts
@@ -0,0 +1,23 @@
+import type { ComponentPropsWithRef, ReactNode } from "react";
+
+export type ButtonVariant = "contained" | "outlined" | "ghost";
+export type ButtonState = "idle" | "active" | "loading" | "disabled";
+export type ButtonColor =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "warning"
+  | "error"
+  | "info";
+export type ButtonSize = "small" | "medium" | "large";
+
+export interface ButtonProps extends ComponentPropsWithRef<"button"> {
+  variant?: ButtonVariant;
+  state?: ButtonState;
+  color?: ButtonColor;
+  size?: ButtonSize;
+  fullWidth?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  loadingText?: string;
+}
