7178fb29215bfa8f3ff8530b7668ccc32339561d
Basel Ghonaim <baselghonaim@gmail.com>
2026-05-08 01:23:58 +0300

    feat: change localhost to "http://localhost:5173"

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,7 +25,7 @@ export const app = express();
 
 // ─── Global Middleware ───────────────────────────────────────────────────────
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
