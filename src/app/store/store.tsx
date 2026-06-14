ced553260735c735382803157f18a2d026acd0ea
Basel-Ghonaim <baselghonaim@gmail.com>
2026-06-14 19:13:54 +0300

    chore(store): register rtk query reducer and middleware

--- src/app/store/store.tsx
+++ src/app/store/store.tsx
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../../modules/auth/store";
+import { baseApi } from "../../shared/rtk-query/baseApi";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
 
 export const reduxStore = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
+
+setupListeners(reduxStore.dispatch);
 
 export type RootState = ReturnType<typeof reduxStore.getState>;
 export type AppDispatch = typeof reduxStore.dispatch;
